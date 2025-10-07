0f25ee6a92ef413d8abc099ce2e472288099a846
ryanmart25 <80070431+ryanmart25@users.noreply.github.com>
2025-10-07 13:58:18 -0700

    added helper methods

---
+++ helper_methods.py
@@ -0,0 +1,98 @@
+from collections.abc import Sequence
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+# Encode text values to dummy variables(i.e. [1,0,0],[0,1,0],[0,0,1] for red,green,blue)
+def encode_text_dummy(df, name):
+    dummies = pd.get_dummies(df[name])
+    for x in dummies.columns:
+        dummy_name = "{}-{}".format(name, x)
+        df[dummy_name] = dummies[x]
+    df.drop(name, axis=1, inplace=True)
+
+
+# Encode text values to indexes(i.e. [1],[2],[3] for red,green,blue).
+def encode_text_index(df, name):
+    le = preprocessing.LabelEncoder()
+    df[name] = le.fit_transform(df[name])
+    return le.classes_
+
+
+# Encode a numeric column as zscores
+def encode_numeric_zscore(df, name, mean=None, sd=None):
+    if mean is None:
+        mean = df[name].mean()
+
+    if sd is None:
+        sd = df[name].std()
+
+    df[name] = (df[name] - mean) / sd
+
+
+# Convert all missing values in the specified column to the median
+def missing_median(df, name):
+    med = df[name].median()
+    df[name] = df[name].fillna(med)
+
+
+# Convert all missing values in the specified column to the default
+def missing_default(df, name, default_value):
+    df[name] = df[name].fillna(default_value)
+
+
+# Convert a Pandas dataframe to the x,y inputs that TensorFlow needs
+def to_xy(df, target):
+    result = []
+    for x in df.columns:
+        if x != target:
+            result.append(x)
+    # find out the type of the target column. 
+    target_type = df[target].dtypes
+    target_type = target_type[0] if isinstance(target_type, Sequence) else target_type
+    # Encode to int for classification, float otherwise. TensorFlow likes 32 bits.
+    if target_type in (np.int64, np.int32):
+        # Classification
+        dummies = pd.get_dummies(df[target])
+        return df[result].values.astype(np.float32), dummies.values.astype(np.float32)
+    else:
+        # Regression
+        return df[result].values.astype(np.float32), df[target].values.astype(np.float32)
+
+# Nicely formatted time string
+def hms_string(sec_elapsed):
+    h = int(sec_elapsed / (60 * 60))
+    m = int((sec_elapsed % (60 * 60)) / 60)
+    s = sec_elapsed % 60
+    return "{}:{:>02}:{:>05.2f}".format(h, m, s)
+
+
+# Regression chart.
+def chart_regression(pred,y,sort=True):
+    t = pd.DataFrame({'pred' : pred, 'y' : y.flatten()})
+    if sort:
+        t.sort_values(by=['y'],inplace=True)
+    a = plt.plot(t['y'].tolist(),label='expected')
+    b = plt.plot(t['pred'].tolist(),label='prediction')
+    plt.ylabel('output')
+    plt.legend()
+    plt.show()
+
+# Remove all rows where the specified column is +/- sd standard deviations
+def remove_outliers(df, name, sd):
+    drop_rows = df.index[(np.abs(df[name] - df[name].mean()) >= (sd * df[name].std()))]
+    df.drop(drop_rows, axis=0, inplace=True)
+
+
+# Encode a column to a range between normalized_low and normalized_high.
+def encode_numeric_range(df, name, normalized_low=-1, normalized_high=1,
+                         data_low=None, data_high=None):
+    if data_low is None:
+        data_low = min(df[name])
+        data_high = max(df[name])
+
+    df[name] = ((df[name] - data_low) / (data_high - data_low)) \
+               * (normalized_high - normalized_low) + normalized_low
+
